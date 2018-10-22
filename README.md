b88eda0c027c0628f2eb552b79ea8cd362b1fbbc
Rafal Markiewicz <r.markiewicz@gmail.com>
2018-10-22 20:13:19 +0200

    Minor changes #1

--- README.md
+++ README.md
@@ -1,24 +1,27 @@
 # armbian-hass
-Home Assistant image for OrangePi 2 based on Armbian.
+This is customization script for Home Assistant installation on Armbian distro.
+Tested on OrangePI 2 and OrangePI PC.
 
 # Installation
-- Follow building Armbian guide: https://docs.armbian.com/Developer-Guide_Build-Preparation to clone git repo
-- After repo is cloned, edit `userpatches/customize-image.sh` and put:
+- Execute commands:
    ```shell
    wget -O /tmp/customize-image-hass.sh https://raw.githubusercontent.com/rufik/armbian-hass/master/customize-image-hass.sh
-   chmod ugo+x /tmp/customize-image-hass.sh
-   bash -c /tmp/customize-image-hass.sh
+   chmod ug+x /tmp/customize-image-hass.sh
+   sudo /tmp/customize-image-hass.sh
    ```
-   It will perform customization with HASSIO and Home Assistant installation with Docker.
-- After image is built just burn it into SD CARD and run your OrangePI SBC.
+   or use -h option to set hostname:
+   ```shell
+   sudo /tmp/customize-image-hass.sh -h opi2
+   ```
+   It will perform customization with Home Assistant installation using Docker.
+- After that machine restart is required if the hostname is changed.
  
 # Running
 When your OPI is up&running just login via ssh and perform:
 ```shell
-sudo systemctl start hassio-supervisor.service
 sudo docker-compose -f /etc/docker/docker-compose-portainer.yml up -d
 ```
-Then go to http://<ip>:9000 for Portainer, you should see portainer and hassio supervisor containers running. Start home-assistant container also.
+Then go to http://<ip>:9000 for Portainer, you should see portainer with containers. Start them one by one: mqtt, influxdb, grafana, home-assistant.
 
 Voila!
  
--- customize-image-hass.sh
+++ customize-image-hass.sh
@@ -1,58 +1,153 @@
 #!/bin/bash
 
-echo "******** -< Starting HASS customization >- ********"
+# from https://github.com/armbian/build/blob/master/lib/general.sh
+print_info()
+{
+  echo "Displaying message: $@" >> hass_customizer.log
+  local tmp=""
+  [[ -n $2 ]] && tmp="[\e[0;33m $2 \x1B[0m]"
 
+  case $3 in
+    err)
+    echo -e "[\e[0;31m error \x1B[0m] $1 $tmp"
+    ;;
+
+    warn)
+    echo -e "[\e[0;35m warn \x1B[0m] $1 $tmp"
+    ;;
+
+    ext)
+    echo -e "[\e[0;32m o.k. \x1B[0m] \e[1;32m$1\x1B[0m $tmp"
+    ;;
+
+    info)
+    echo -e "[\e[0;32m o.k. \x1B[0m] $1 $tmp"
+    ;;
+
+    *)
+    echo -e "[\e[0;32m info \x1B[0m] $1 $tmp"
+    ;;
+  esac
+}
+
+if [ $(id -u) != 0 ]; then
+	print_info "This script has to be run as root!" "" "err"
+	exit 1
+fi
+
+
+#some variables
+BASE_DIR="/opt/has"
+DOCKER_COMPOSE_DIR="${BASE_DIR}"
+
+print_info "******** -< Starting HASS customization >- ********" "" "ext"
 #user creation is armbian's first run feature
 
-#how to avoid question about decision of config files?
-apt-get update && apt-get -y upgrade
+#fix improper min cpu freq for H3
+sed -i 's/408000/480000/g' /etc/default/cpufrequtils
+service cpufrequtils restart
 
-# reboot here if kernel has changed?
+print_info "Updating OS..."
+apt-get -q update && apt-get -y upgrade
 
-echo "Installing required packages ..."
-apt-get -y install mc ccze jq avahi-daemon screen
+#make sure that OS upgrade does not recover faulty min cpu freq
+sed -i 's/408000/480000/g' /etc/default/cpufrequtils
+service cpufrequtils restart
+
+systemctl stop unattended-upgrades
+systemctl disable unattended-upgrades
+
+print_info "Installing required packages and armbianmonitor..."
+apt-get -q -y install mc ccze jq avahi-daemon screen telnet
 armbianmonitor -r
 
-echo "Setting red LED as hearbeat on system start ..."
+print_info "Setting red LED as hearbeat on system start ..."
 sed -i 's/exit 0//g' /etc/rc.local
 echo 'RESULT=`echo "heartbeat" > /sys/class/leds/orangepi\:red\:status/trigger`' >> /etc/rc.local
 echo 'exit 0' >> /etc/rc.local
 
-echo "Setting hostname 'opi2' ..."
-hostname opi2
-echo 'opi2' >> /etc/hostname
+while getopts ":h:" opt; do
+	case "$opt" in
+	h)
+		print_info "Setting hostname '$OPTARG' ..."
+		hostname $OPTARG
+		echo "$OPTARG" > /etc/hostname
+	;;
+	\?)
+		print_info "Invalid option: -$OPTARG. Skipping." "" "warn"
+		;;
+	:)
+		print_info "Option -$OPTARG requires an argument. Skipping" "" "warn"
+		;;
+	esac
+done
 
 
-echo "Installing Docker CE ..."
+print_info "Installing Docker CE ..."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=armhf] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get -y install docker-ce docker-compose
+apt-get -q update
+apt-get -q -y install docker-ce docker-compose
+systemctl enable docker
+print_info "" "Done." "info"
 
-echo "Installing HASSIO Supervisor ..."
-wget -O /tmp/hassio_install.sh https://raw.githubusercontent.com/home-assistant/hassio-build/master/install/hassio_install.sh
-chmod ugo+x /tmp/hassio_install.sh
-bash -c '/tmp/hassio_install.sh -m armhf'
+print_info "Adding user 'has'..."
+useradd -m has
+print_info "Setting password for user has - you will be prompted for password!"
+passwd has
 
-echo "Pulling Portainer, Home Assistant and Mosquitto images ..."
-mkdir -p /opt/has/config
-mkdir -p /opt/has/mqtt/data
-mkdir -p /opt/has/mqtt/logs
-wget -O /etc/docker/docker-compose-portainer.yml https://raw.githubusercontent.com/rufik/armbian-hass/master/docker-compose-portainer.yml
-wget -O /etc/docker/docker-compose-hass.yml https://raw.githubusercontent.com/rufik/armbian-hass/master/docker-compose-hass.yml
-wget -O /etc/docker/docker-compose-mqtt.yml https://raw.githubusercontent.com/rufik/armbian-hass/master/docker-compose-mqtt.yml
-#docker-compose -f /etc/docker/docker-compose-portainer.yml pull
-docker-compose -f /etc/docker/docker-compose-hass.yml pull
-docker-compose -f /etc/docker/docker-compose-mqtt.yml pull
+print_info "Preparing folders in $BASE_DIR"
+mkdir -p $BASE_DIR/config
+mkdir -p $BASE_DIR/addons
+mkdir -p $BASE_DIR/mqtt/data
+mkdir -p $BASE_DIR/mqtt/logs
+mkdir -p $BASE_DIR/mqtt/conf
+mkdir -p $BASE_DIR/influxdb
+mkdir -p $BASE_DIR/mariadb/data
+mkdir -p $BASE_DIR/mariadb/config
+#set user "has" as owner
+chown -R has.root $BASE_DIR/opt/has
+#chmod ug+rwx $BASE_DIR/mqtt/logs/
 
-echo "Creating Portainer, Home Assistant and Mosquitto containers ..."
-#docker-compose -f /etc/docker/docker-compose-portainer.yml create
-docker-compose -f /etc/docker/docker-compose-hass.yml create
-docker-compose -f /etc/docker/docker-compose-mqtt.yml create
+print_info "Downloading MQTT config files..."
+wget -q -O /$BASE_DIR/mqtt/conf/options.json https://raw.githubusercontent.com/rufik/armbian-hass/master/mqtt/options.json
+wget -q -O $BASE_DIR/mqtt/conf/mosquitto.conf https://raw.githubusercontent.com/rufik/armbian-hass/master/mqtt/mosquitto.conf
+#prevent mosquitto from overwriting config file
+chmod ugo-w $BASE_DIR/mqtt/conf/mosquitto.conf
+#wget -O /opt/has/addons/configurator-settings.conf https://raw.githubusercontent.com/rufik/armbian-hass/master/addons/configurator-settings.conf
+print_info "" "Done." "info"
 
-echo "Starting Portainer..."
-docker-compose -f /etc/docker/docker-compose-portainer.yml up -d
-echo "Done."
+print_info "Downloading HASS, MariaDB and InfluxDB config files..."
+wget -q -O $BASE_DIR/influxdb/influxdb.conf https://raw.githubusercontent.com/rufik/armbian-hass/master/influxdb/influxdb.conf
+wget -q -O $BASE_DIR/config/configuration.yaml https://raw.githubusercontent.com/rufik/armbian-hass/master/hass/configuration.yaml
+wget -q -O $BASE_DIR/mariadb/config/my.cnf https://raw.githubusercontent.com/rufik/armbian-hass/master/mariadb/my.cnf
+print_info "" "Done." "info"
 
-echo "******** -< End of HASS customization >- ********"
+
+print_info "Downloading docker-compose files into $DOCKER_COMPOSE_DIR dir..."
+wget -q -O $DOCKER_COMPOSE_DIR/docker-compose-portainer.yml https://raw.githubusercontent.com/rufik/armbian-hass/master/docker-compose-portainer.yml
+wget -q -O $DOCKER_COMPOSE_DIR/docker-compose-hass.yml https://raw.githubusercontent.com/rufik/armbian-hass/master/docker-compose-hass.yml
+print_info "" "Done." "info"
+
+HAS_UID=`id -u has`
+print_info "Patching docker-compose files using 'has' user UID=$HAS_UID"
+sed -i -e 's\user: has\user: "$HAS_UID"\g' $DOCKER_COMPOSE_DIR/docker-compose-hass.yml
+print_info "" "Done." "info"
+
+
+print_info "Pulling & creating docker images..."
+docker-compose -f $DOCKER_COMPOSE_DIR/docker-compose-portainer.yml pull
+docker-compose -f $DOCKER_COMPOSE_DIR/docker-compose-hass.yml pull
+docker-compose -f $DOCKER_COMPOSE_DIR/docker-compose-portainer.yml up --no-start
+docker-compose -f $DOCKER_COMPOSE_DIR/docker-compose-hass.yml up --no-start
+print_info "" "Done." "info"
+
+#echo "Starting Portainer..."
+#docker-compose -f $DOCKER_COMPOSE_DIR/docker-compose-portainer.yml up -d
+#echo "Done."
+
+print_info "******** -< End of HASS customization >- ********" "" "ext"
+HOSTNAME=`hostname -s`
+print_info "It's preferred to reboot your machine and then start Portainer. You can find it using your browser at http://$HOSTNAME:9000"
+print_info "You can start Portainer by command: sudo docker-compose -f $DOCKER_COMPOSE_DIR/docker-compose-portainer.yml up -d"
 echo ""
